b1c0d59c82c1f4593d676fa6b72f72f8c2232d71
meloviliju <ritterderschwerter178@gmail.com>
2023-05-13 01:16:34 +0900

    add: 外部リンクの配列からリストを生成するcomponentを追加
    pagesへのべた書きをやめ、配列による管理へ変更

--- pages/tec-leti-julo.tsx
+++ pages/tec-leti-julo.tsx
@@ -1,61 +1,31 @@
-import Link from "next/link";
 import Head from "next/head";
-import { Header } from "src/components/header";
-import { Footer } from "src/components/footer";
 import { Title, Description } from "src/components/common";
-import styled from "@emotion/styled";
+import { PostOGP } from "src/components/common/PostOGP";
+import usePageTitle from "src/hooks/usePageTitle";
+import { ExternalLinkList } from "src/components/common/ExternalLinkList";
+import { externalLinkData } from "public/linkdata/tec-leti-julo";
 
 const Index = () => {
+  usePageTitle("外部リンク");
   return (
     <div>
       <Head>
-        <title>外部リンク | AIL MO LETI CEP</title>
+        <PostOGP
+          title="外部リンク"
+          type="article"
+          url="https://ail-mo-leti-cep.github.io/tec-leti-julo"
+          description="アイル共和国文化省およびその担当する事柄に関するリンクを紹介しています。"
+        />
       </Head>
-      <Header />
       <main>
         <Title>外部リンク</Title>
         <Description>
-          このページでは、アイル共和国文化省およびその担当する事柄に関するリンクを紹介しています。
+          アイル共和国文化省およびその担当する事柄に関するリンクを紹介しています。
           <br />
           メタ的な説明が含まれているため、ご注意ください。
         </Description>
-        <ul>
-          <li>
-            <Link
-              href="https://sites.google.com/view/jurli/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              総合創作界隈「悠里」公式サイト
-            </Link>
-            <br />
-            アイル共和国やファイクレオネなど架空の世界およびその詳細な設定に至るまでを総合的に創作している『総合創作界隈「悠里」』の公式サイトです。
-          </li>
-          <li>
-            <Link
-              href="https://sites.google.com/view/cet2kaik/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              日本机戦連盟公式サイト
-            </Link>
-            <br />
-            アイル共和国の伝統遊戯である「机戦」の日本における公式組織である『日本机戦連盟』の公式サイトです。
-          </li>
-          <li>
-            <Link
-              href="https://sozysozbot.github.io/cerke/AIL%20PANIT%20LETI%20CETKAIK%20LETI%20KULANTE.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              AIL PANIT LETI CETKAIK LETI KULANTE
-            </Link>
-            <br />
-            アイル共和国文化省が統一ルールを制定した「官定机戦」のルールブック原本です。アイル共和国の公用語の一つである東島通商語で書かれています。
-          </li>
-        </ul>
+        <ExternalLinkList linkDataList={externalLinkData} />
       </main>
-      <Footer />
     </div>
   );
 };
---
+++ public/linkdata/tec-leti-julo.ts
@@ -0,0 +1,23 @@
+export const externalLinkData = [
+  {
+    title: "総合創作界隈「悠里」公式サイト",
+    url: "https://sites.google.com/view/jurli/",
+    description: [
+      "アイル共和国やファイクレオネなど架空の世界およびその詳細な設定に至るまでを総合的に創作している『総合創作界隈「悠里」』の公式サイトです。",
+    ],
+  },
+  {
+    title: "日本机戦連盟公式サイト",
+    url: "https://sites.google.com/view/cet2kaik/",
+    description: [
+      "アイル共和国の伝統遊戯である「机戦」の日本における公式組織である『日本机戦連盟』の公式サイトです。",
+    ],
+  },
+  {
+    title: "AIL PANIT LETI CETKAIK LETI KULANTE",
+    url: "https://sozysozbot.github.io/cerke/AIL%20PANIT%20LETI%20CETKAIK%20LETI%20KULANTE.pdf",
+    description: [
+      "アイル共和国文化省が統一ルールを制定した「官定机戦」のルールブック原本です。アイル共和国の公用語の一つである東島通商語で書かれています。",
+    ],
+  },
+];
